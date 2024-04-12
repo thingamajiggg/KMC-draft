5f3ec17ffc1c0ce7eee721ca55f6bc7ca578ef62
Wei Keong <wkeong_00@hotmail.com>
2024-04-12 16:56:18 +0800

    merge contact

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+        <div className="contact-col">
+            <h3>Send us message</h3>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
