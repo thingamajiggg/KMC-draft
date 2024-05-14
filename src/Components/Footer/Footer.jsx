e167035be55bf1d51b7e40120b834474824e6380
Wei Keong <wkeong_00@hotmail.com>
2024-05-14 17:32:41 +0800

    merge code 14/may

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faFacebook, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
     return (
@@ -8,8 +11,21 @@ const Footer = () => {
             <ul>
                 <li>Terms of Service</li>
                 <li>Privacy policy</li>
-
             </ul>
+            <div className='social-icons'>
+                <a href="https://www.instagram.com/thingamajigg_/" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faInstagram} />
+                </a>
+                <a href="https://www.facebook.com/profile.php?id=61558628079399" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faFacebook} />
+                </a>
+                <a href="https://wa.me/+60165177619" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faWhatsapp} />
+                </a>
+                <a href="https://www.google.com/maps/place/KMC+63+Trading/@3.0389174,101.7488713,15z/data=!4m6!3m5!1s0x31cc3543def799d1:0x4badda68a84593da!8m2!3d3.0389174!4d101.7488713!16s%2Fg%2F11vx64wxn1?hl=en&entry=ttu" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faMapMarkerAlt} />
+                </a>
+            </div>
         </div>
     )
 }
