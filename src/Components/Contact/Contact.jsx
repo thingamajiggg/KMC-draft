7d1fa6b5124988553812aff6b8a505c10bf75413
Wei Keong <wkeong_00@hotmail.com>
2024-06-07 15:32:04 +0800

    merge photo service and map

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -46,10 +46,20 @@ const Contact = () => {
                     <li><img src={mailicon} alt=""/>
                         kmc63trading@gmail.com</li>
                     <li><img src={phoneIcon} alt=""/>
-                        +60123322898</li>
+                        +60163585326</li>
                     <li><img src={locaIcon} alt=""/>
                         No.1, Jalan Com Industrial Park2/1, Com Industrial Park 2, kawasan Perindustrian
                         Kg.Baru Balakong, 43300 Seri Kembangan, Selangor Darul Ehsan. Malaysia</li>
+                    <li>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m22!1m8!1m3!1d3984.20780118723!2d101.74628347497074!3d3.038879646936964!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d3.0388401999999997!2d101.74874369999999!4m5!1s0x31cc3543def799d1%3A0x4badda68a84593da!2skmc63trading!3m2!1d3.0389174!2d101.74887129999999!5e0!3m2!1sen!2smy!4v1717744066121!5m2!1sen!2smy"
+                            width="400"
+                            height="300"
+                            style={{border:0}}
+                            allowfullscreen
+                            loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </li>
                 </ul>
             </div>
             <div className="contact-col">
--- src/Components/ServicePage/ServiceCustom.jsx
+++ src/Components/ServicePage/ServiceCustom.jsx
@@ -1,78 +1,102 @@
 import React from "react";
 import "./ServiceCustom.css";
-import coatingImage1 from "../../Assets/coating11.jpg";
-import coatingImage2 from "../../Assets/coating.jpg";
-import coatingImage3 from "../../Assets/coating1.jpg";
-import serviceImage1 from "../../Assets/company1.jpg";
-import serviceImage2 from "../../Assets/company2.jpg";
-import serviceImage3 from "../../Assets/company3.jpg";
-import custom1 from '../../Assets/company4.jpg'
-import custom3 from '../../Assets/custom3.jpg'
+import oldtray from '../../Assets/oldbakingtray.png'
+import completetray from '../../Assets/complatetray.png'
+import bakeremove1 from '../../Assets/bakeremove1.png'
+import bakeremove2 from '../../Assets/bakeremove 2.png'
+import bakeremove3 from '../../Assets/bakeremove3.png'
+import reconbake from '../../Assets/recontray.png'
+import trayremove1 from '../../Assets/trayremove1.png'
 
 const ServiceCustom = () => {
     return (
         <div className="servicescustom">
             <div className="servicetitle">
                 <h2>Coating Service</h2>
-                <p>我们的涂层服务，达到最高的卫生和效率标准，也非常耐用， 如果您的盘子有损坏需要维护或重新涂层，我们都能够为您
-                    进行维护以及重新上色，以延长您的烤盘的性能与寿命。如果 有问题请立即向我们联系！</p>
-                <p>Our coating service meets the highest standards of hygiene and efficiency and
-                    is highly durable. If your trays are damaged and require maintenance or
-                    recoating, we can provide maintenance and recoating services to extend the
-                    performance and lifespan of your trays. Please contact us immediately if you
-                    have any issues!</p>
+                <p>我们的涂层服务包括 :</p>
+                    <li>翻新旧烤盘</li>
+                    <li>为其重新喷上涂料</li>
+                    <li>漆料使用水性双层涂料</li>
+                <p>我们使用的漆料是以聚四氟乙烯为基本的不占涂料，它能让你的产品:</p>
+                    <li>具有高耐蚀性</li>
+                    <li>耐高温</li>
+                    <li>优越的不粘性</li>
+                    <li>良好的防水性</li>
+                
             </div>
 
             <div className="servicesrow">
                 <div className="servicecus">
-                    <img src={coatingImage1} alt=""/>
+                    <img src={oldtray} alt=""/>
                     <div className="captioncus">
-                        <p>Coating service 1</p>
+                        <p>Old Baking Tray</p>
+                    </div>
+                    <div className="servicestatus">
+                        <p>Before</p>
                     </div>
                 </div>
                 <div className="servicecus">
-                    <img src={coatingImage2} alt=""/>
+                    <img src={completetray} alt=""/>
                     <div className="captioncus">
-                        <p>Coating service 2</p>
+                        <p>Baking trays after coating</p>
                     </div>
-                </div>
-                <div className="servicecus">
-                    <img src={coatingImage3} alt=""/>
-                    <div className="captioncus">
-                        <p>Coating service 3</p>
+                    <div className="servicestatus">
+                        <p>After</p>
                     </div>
                 </div>
             </div>
 
             <div className="servicetitle">
                 <h2>Customize services</h2>
-                <p>关于我们的定制烤盘服务，无论是不同的形状、尺寸还是图案， 我们都能够根据客户的需求来制作烤盘。如果您对烤盘有特定的需求
-                    却苦于找不到合适的定制化烤盘，请务必联系我们。我们一定能提供 您所需要的定制烤盘。
-                </p>
-                <p>Regarding our customized bun tray services, we can produce trays 
-                    based on customer requirements, whether it's different shapes, sizes, 
-                    or patterns. If you have specific needs for bun trays and are struggling to 
-                    find customized solutions, please don't hesitate to contact us.
-                     We can definitely provide the customized trays you need.</p>
+                <p>客制化烤盘服务包括 :</p>
+                    <li>接受各种尺寸的订做</li>
+                    <li>各种形状，图案，皆可制作</li>
+                    <li>材料也包括白钢，铝合金等等</li>
+                <p>我们的烤盘也提供几项功能:</p>
+                    <li>具有高耐蚀性</li>
+                    <li>耐高温</li>
+                    <li>优越的不粘性</li>
+                    <li>良好的防水性</li>
             </div>
 
             <div className="servicesrow">
                 <div className="servicecus">
-                    <img src={custom1} alt=""/>
+                    <img src={reconbake} alt=""/>
                     <div className="captioncus">
-                        <p>Coating service 1</p>
+                        <p>Customize baking trays</p>
                     </div>
                 </div>
                 <div className="servicecus">
-                    <img src={serviceImage2} alt=""/>
+                    <img src={completetray} alt=""/>
                     <div className="captioncus">
-                        <p>Coating service 2</p>
+                        <p>Customize baking trays</p>
                     </div>
                 </div>
                 <div className="servicecus">
-                    <img src={custom3} alt=""/>
+                    <img src={bakeremove1} alt=""/>
                     <div className="captioncus">
-                        <p>Coating service 3</p>
+                        <p>Customize baking trays</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="servicesrow">
+                <div className="servicecus">
+                    <img src={bakeremove2} alt=""/>
+                    <div className="captioncus">
+                        <p>Customize baking trays</p>
+                    </div>
+                </div>
+                <div className="servicecus">
+                    <img src={bakeremove3} alt=""/>
+                    <div className="captioncus">
+                        <p>Customize baking trays</p>
+                    </div>
+                </div>
+                <div className="servicecus">
+                    <img src={trayremove1} alt=""/>
+                    <div className="captioncus">
+                        <p>Customize baking trays</p>
                     </div>
                 </div>
             </div>
