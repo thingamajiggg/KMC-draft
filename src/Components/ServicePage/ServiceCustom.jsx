a4455ffa2699ed89631e920cf95a9fb6e8767918
Wei Keong <wkeong_00@hotmail.com>
2024-06-20 17:09:58 +0800

    merge 10june

--- src/Components/ServicePage/ServiceCustom.jsx
+++ src/Components/ServicePage/ServiceCustom.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import "./ServiceCustom.css";
 import oldtray from '../../Assets/oldbakingtray.png'
 import completetray from '../../Assets/complatetray.png'
-import bakeremove1 from '../../Assets/bakeremove1.png'
-import bakeremove2 from '../../Assets/bakeremove 2.png'
+import bakeremove1 from '../../Assets/example1-removebg.png'
+import bakeremove2 from '../../Assets/example6-removebg.png'
+import style from '../../Assets/styletray.png'
 import bakeremove3 from '../../Assets/bakeremove3.png'
-import reconbake from '../../Assets/recontray.png'
 import trayremove1 from '../../Assets/trayremove1.png'
 
 const ServiceCustom = () => {
     return (
         <div className="servicescustom">
             <div className="servicetitle">
-                <h2>Coating Service</h2>
+                <h2>喷漆涂层服务</h2>
                 <p>我们的涂层服务包括 :</p>
                     <li>翻新旧烤盘</li>
                     <li>为其重新喷上涂料</li>
@@ -22,6 +22,16 @@ const ServiceCustom = () => {
                     <li>耐高温</li>
                     <li>优越的不粘性</li>
                     <li>良好的防水性</li>
+                    <h2>Coating Service</h2>
+                <p>Our coating services include:</p>
+                    <li>Refurbishing old baking trays</li>
+                    <li>Spray a new coat of paint</li>
+                    <li>Using water-based double-layer coatings</li>
+                <p>The paint we use is a non-stick coating based on polytetrafluoroethylene (PTFE), which provides your products with:</p>
+                    <li>High corrosion resistance</li>
+                    <li>Heat resistance</li>
+                    <li>Excellent non-stick properties</li>
+                    <li>Good waterproofing</li>
                 
             </div>
 
@@ -45,9 +55,9 @@ const ServiceCustom = () => {
                     </div>
                 </div>
             </div>
-
+            <hr/>
             <div className="servicetitle">
-                <h2>Customize services</h2>
+                <h2>客制化烤盘服务</h2>
                 <p>客制化烤盘服务包括 :</p>
                     <li>接受各种尺寸的订做</li>
                     <li>各种形状，图案，皆可制作</li>
@@ -57,11 +67,21 @@ const ServiceCustom = () => {
                     <li>耐高温</li>
                     <li>优越的不粘性</li>
                     <li>良好的防水性</li>
+                    <h2>Customize services</h2>
+                <p>Customization services for baking trays include:</p>
+                    <li>Accepting custom orders of various sizes</li>
+                    <li>Making various shapes and patterns</li>
+                    <li>Materials include stainless steel, aluminum alloy, and more</li>
+                <p>Our baking trays also provide several features:</p>
+                    <li>High corrosion resistance</li>
+                    <li>Heat resistance</li>
+                    <li>Excellent non-stick properties</li>
+                    <li>Good waterproofing</li>
             </div>
 
             <div className="servicesrow">
                 <div className="servicecus">
-                    <img src={reconbake} alt=""/>
+                    <img src={style} alt=""/>
                     <div className="captioncus">
                         <p>Customize baking trays</p>
                     </div>
@@ -100,6 +120,7 @@ const ServiceCustom = () => {
                     </div>
                 </div>
             </div>
+            <hr/>
         </div>
     );
 };
